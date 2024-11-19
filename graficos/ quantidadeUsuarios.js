2150a1422b97b57dc78ea17f58a095d8bb02a008
dudu7xs <165044299+dudu7xs@users.noreply.github.com>
2024-11-19 10:52:57 -0300

    aa

--- graficos/ quantidadeUsuarios.js
+++ graficos/ quantidadeUsuarios.js
@@ -1,59 +1,45 @@
-import { getCSS, tickConfig } from "./common.js"
+import { getCSS, criarGrafico, incluirTexto } from "./common.js"
 
-async function quantidadeUsuariosPorRede() {
-    const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
+async function redesFavoritasMundo() {
+    const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/redes-favoritas.json'
     const res = await fetch(url)
     const dados = await res.json()
-    const nomeDasRedes = Object.keys(dados)
-    const quantidadeDeUsuarios = Object.values(dados)
+    const redes = Object.keys(dados)
+    const valores = Object.values(dados)
 
     const data = [
         {
-            x: nomeDasRedes, 
-            y: quantidadeDeUsuarios, 
-            type: 'bar',
-            marker: {
-                color: getCSS('--primary-color')
-            }
+            values: valores,
+            labels: redes,
+            type: 'pie',
+            textinfo: 'label+percent'
         }
     ]
 
-    const laytout = {
+    const layout = {
         plot_bgcolor: getCSS('--bg-color'),
         paper_bgcolor: getCSS('--bg-color'),
+        height: 700,
         title: {
-            text: 'Redes sociais com mais usuários',
+            text: 'Redes sociais que os usuários mais gostam',
             x: 0,
             font: {
                 color: getCSS('--primary-color'),
-                size: 30,
-                font: getCSS('--font')
+                family: getCSS('--font'),
+                size: 30
             }
         },
-        xaxis: {
-            tickfont: tickConfig,
-            title: {
-                text: 'Nome das redes',
-                font: {
-                    color: getCSS('--secondary-color')
-                }
-            }
-        },
-        yaxis: {
-            tickfont: tickConfig,
-            title: {
-                text: 'Bilhões de usuários ativos',
-                font: {
-                    color: getCSS('--secondary-color')
-                }
+        legend: {
+            font: {
+                color: getCSS('--primary-color'),
+                size: 16
             }
         }
     }
 
-    const grafico = document.createElement('div')
-    grafico.className = 'grafico'
-    document.getElementById('graficos-container').appendChild(grafico)
-    Plotly.newPlot(grafico, data, laytout)
+    criarGrafico(data, layout)
+
+    incluirTexto(`Embora o <span>Instagram</span> ocupe a quarta posição em termos de número total de usuários entre as redes sociais, destaca-se como a <span>preferida pelos usuários</span>. Supera até mesmo o <span>Facebook</span>, a plataforma com mais usuários, sendo a terceira opção mais apreciada pelos usuários. <br>Essa preferência evidencia a forte conexão e apreço que as pessoas têm pelo Instagram em comparação com outras redes sociais`)
 }
 
-quantidadeUsuariosPorRede()
+redesFavoritasMundo()
